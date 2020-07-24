ffe57488dd81c9886e9466b662a68d20bfe6c1d1
Bischu99 <kinggodsugeun@gmail.com>
2020-07-24 14:01:56 +0900

    WEB

---
+++ 계산기3/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace 계산기3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.Decimal = new System.Windows.Forms.Button();
+			this.NumAdd = new System.Windows.Forms.Button();
+			this.NumSub = new System.Windows.Forms.Button();
+			this.NumDivide = new System.Windows.Forms.Button();
+			this.NumMultiple = new System.Windows.Forms.Button();
+			this.textBoxWorkspace = new System.Windows.Forms.TextBox();
+			this.textBoxLast = new System.Windows.Forms.TextBox();
+			this.buttonEqual = new System.Windows.Forms.Button();
+			this.Reset = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(208, 195);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(31, 30);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(245, 195);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(31, 30);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(282, 195);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(31, 30);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(208, 159);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(31, 30);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(245, 159);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(31, 30);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(282, 159);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(31, 30);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(208, 123);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(31, 30);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(245, 123);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(31, 30);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(282, 123);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(31, 30);
+			this.button9.TabIndex = 8;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = true;
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(208, 231);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(31, 30);
+			this.button10.TabIndex = 9;
+			this.button10.Text = "0";
+			this.button10.UseVisualStyleBackColor = true;
+			// 
+			// Decimal
+			// 
+			this.Decimal.Location = new System.Drawing.Point(245, 231);
+			this.Decimal.Name = "Decimal";
+			this.Decimal.Size = new System.Drawing.Size(31, 30);
+			this.Decimal.TabIndex = 10;
+			this.Decimal.Text = ".";
+			this.Decimal.UseVisualStyleBackColor = true;
+			// 
+			// NumAdd
+			// 
+			this.NumAdd.Location = new System.Drawing.Point(321, 231);
+			this.NumAdd.Name = "NumAdd";
+			this.NumAdd.Size = new System.Drawing.Size(33, 30);
+			this.NumAdd.TabIndex = 11;
+			this.NumAdd.Text = "+";
+			this.NumAdd.UseVisualStyleBackColor = true;
+			// 
+			// NumSub
+			// 
+			this.NumSub.Location = new System.Drawing.Point(321, 195);
+			this.NumSub.Name = "NumSub";
+			this.NumSub.Size = new System.Drawing.Size(31, 30);
+			this.NumSub.TabIndex = 12;
+			this.NumSub.Text = "-";
+			this.NumSub.UseVisualStyleBackColor = true;
+			// 
+			// NumDivide
+			// 
+			this.NumDivide.Location = new System.Drawing.Point(321, 159);
+			this.NumDivide.Name = "NumDivide";
+			this.NumDivide.Size = new System.Drawing.Size(31, 30);
+			this.NumDivide.TabIndex = 13;
+			this.NumDivide.Text = "/";
+			this.NumDivide.UseVisualStyleBackColor = true;
+			// 
+			// NumMultiple
+			// 
+			this.NumMultiple.Location = new System.Drawing.Point(321, 123);
+			this.NumMultiple.Name = "NumMultiple";
+			this.NumMultiple.Size = new System.Drawing.Size(31, 30);
+			this.NumMultiple.TabIndex = 14;
+			this.NumMultiple.Text = "*";
+			this.NumMultiple.UseVisualStyleBackColor = true;
+			// 
+			// textBoxWorkspace
+			// 
+			this.textBoxWorkspace.Location = new System.Drawing.Point(208, 96);
+			this.textBoxWorkspace.Name = "textBoxWorkspace";
+			this.textBoxWorkspace.Size = new System.Drawing.Size(142, 21);
+			this.textBoxWorkspace.TabIndex = 16;
+			// 
+			// textBoxLast
+			// 
+			this.textBoxLast.Location = new System.Drawing.Point(252, 69);
+			this.textBoxLast.Name = "textBoxLast";
+			this.textBoxLast.Size = new System.Drawing.Size(100, 21);
+			this.textBoxLast.TabIndex = 17;
+			// 
+			// buttonEqual
+			// 
+			this.buttonEqual.Location = new System.Drawing.Point(284, 231);
+			this.buttonEqual.Name = "buttonEqual";
+			this.buttonEqual.Size = new System.Drawing.Size(31, 30);
+			this.buttonEqual.TabIndex = 18;
+			this.buttonEqual.Text = "=";
+			this.buttonEqual.UseVisualStyleBackColor = true;
+			// 
+			// Reset
+			// 
+			this.Reset.Location = new System.Drawing.Point(208, 69);
+			this.Reset.Name = "Reset";
+			this.Reset.Size = new System.Drawing.Size(43, 23);
+			this.Reset.TabIndex = 19;
+			this.Reset.Text = "reset";
+			this.Reset.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Reset);
+			this.Controls.Add(this.buttonEqual);
+			this.Controls.Add(this.textBoxLast);
+			this.Controls.Add(this.textBoxWorkspace);
+			this.Controls.Add(this.NumMultiple);
+			this.Controls.Add(this.NumDivide);
+			this.Controls.Add(this.NumSub);
+			this.Controls.Add(this.NumAdd);
+			this.Controls.Add(this.Decimal);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button Decimal;
+		private System.Windows.Forms.Button NumAdd;
+		private System.Windows.Forms.Button NumSub;
+		private System.Windows.Forms.Button NumDivide;
+		private System.Windows.Forms.Button NumMultiple;
+		private System.Windows.Forms.TextBox textBoxWorkspace;
+		private System.Windows.Forms.TextBox textBoxLast;
+		private System.Windows.Forms.Button buttonEqual;
+		private System.Windows.Forms.Button Reset;
+	}
+}
+
---
+++ 계산기3/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 계산기3
+{
+	public partial class Form1 : Form
+	{
+		// This determine which variable to change.
+		// If it's 0, affect operand1, if 2, affact operand2
+		int operandChange = 0;
+		// operand1 = null  건드리지 않았으면 널  조건문 비교 하려고 씀 
+		private string operand1 = null;
+		private string operand2 = null;
+		private string operation = null;
+		public Form1()
+		{
+			InitializeComponent();
+			button1.Click += (sender, e) => TextAdd(1);
+			button2.Click += (sender, e) => TextAdd(2);
+			button3.Click += (sender, e) => TextAdd(3);
+			button4.Click += (sender, e) => TextAdd(4);
+			button5.Click += (sender, e) => TextAdd(5);
+			button6.Click += (sender, e) => TextAdd(6);
+			button7.Click += (sender, e) => TextAdd(7);
+			button8.Click += (sender, e) => TextAdd(8);
+			button9.Click += (sender, e) => TextAdd(9);
+			button10.Click += (sender, e) => TextAdd(0);
+
+			NumAdd.Click += (sender, e) => OperationA('+');
+			NumSub.Click += (sender, e) => OperationA('-');
+			NumDivide.Click += (sender, e) => OperationA('/');
+			NumMultiple.Click += (sender, e) => OperationA('*');
+
+			buttonEqual.Click += (sender, e) => Equal_result();
+
+			Reset.Click += (sender, e) => ResetAll();
+			Decimal.Click += (sender, e) => DecimalPoint();
+		}
+
+		private void TextAdd(int num)
+		{
+			if (operandChange == 0)
+			{
+				operand1 += num;
+				textBoxWorkspace.Text += num;
+			}
+			else 
+			{
+				operand2 += num;
+				textBoxWorkspace.Text += num;
+			}
+			
+		}
+		private void OperationA(char Oper)
+		{
+			if (operation == null)
+			{
+				operation += Oper;
+				textBoxWorkspace.Text += Oper;
+			}
+			operandChange = 1;// 연산자 넣어줬으니 operand2에 input number 
+		}
+		private void Equal_result()
+		{
+			double result = 0;
+			switch (operation)
+			{
+				case "+": result = double.Parse(operand1) + double.Parse(operand2); break;
+				case "-": result = double.Parse(operand1) - double.Parse(operand2); break;
+				case "*": result = double.Parse(operand1) * double.Parse(operand2); break;
+				case "/": result = double.Parse(operand1) / double.Parse(operand2); break;
+			};
+
+			textBoxLast.Text = textBoxWorkspace.Text;// 계산식을  textBoxLast 위 쪽에있는 텍스트 박스로 이동
+			textBoxWorkspace.Text = result.ToString();// 계산 결과값으로 초기화
+			
+
+			operand1 = result.ToString();
+			operand2 = null;
+			operation = null;
+
+		}
+		private void ResetAll()
+		{
+			operandChange = 0;
+			operand1 = null;
+			operand2 = null;
+			operation = null;
+			textBoxWorkspace.Clear();
+			textBoxLast.Clear();
+		}
+		private void DecimalPoint()
+		{
+			if (operandChange == 0)
+			{
+				if (operand1.Contains('.') == false)
+				{
+					operand1 += '.';
+					textBoxWorkspace.Text += '.';
+				}
+			}
+			else
+			{
+				if (operand2.Contains('.') == false)
+				{
+					operand2 += '.';
+					textBoxWorkspace.Text += '.';
+				}
+			}
+		}
+
+		
+	}
+}
